e5709ba201f5a6c9433eedeca2266d732d735fb5
harsh kumar jha <115914239+harshguarav@users.noreply.github.com>
2025-07-07 12:01:32 +0530

    Time: 15 ms (81.11%), Space: 91.8 MB (66.08%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        vector<int> leftToRight(prices.size(),0);
-        int minLeft=prices[0];
-        int buy=0;
-        for(int i=0;i<prices.size();i++){
-            
-            buy=max(buy,prices[i]-minLeft);
-            leftToRight[i]=buy;
-            minLeft = min(minLeft, prices[i]);
-            
-        }
-        vector<int> rightToLeft(prices.size(),0);
-        int minRight=prices[prices.size()-1];
-        int sell=0;
-        for(int i=prices.size()-1;i>=0;i--){
-            sell=max(sell,minRight-prices[i]);
-            rightToLeft[i]=sell;
-            minRight = max(minRight, prices[i]);
-        }
-        int result=0;
-        for(int i=0;i<prices.size();i++){
-            result=max(result,leftToRight[i]+rightToLeft[i]);
+    int maxProfit( vector<int>& prices) {
+        int n = prices.size();
+        // if (n == 0 || k == 0) return 0;
 
-        }
-        return result;
+        // // If k is large enough, just do as many transactions as possible (greedy)
+        // if (k >= n / 2) {
+        //     int profit = 0;
+        //     for (int i = 1; i < n; i++) {
+        //         if (prices[i] > prices[i - 1])
+        //             profit += prices[i] - prices[i - 1];
+        //     }
+        //     return profit;
+        // }
 
-        
+        // DP approach
+        vector<vector<int>> dp(2 + 1, vector<int>(n, 0));
+
+        for (int t = 1; t <= 2; t++) {
+            int maxDiff = -prices[0];
+            for (int i = 1; i < n; i++) {
+                dp[t][i] = max(dp[t][i - 1], prices[i] + maxDiff);
+                maxDiff = max(maxDiff, dp[t - 1][i] - prices[i]);
+            }
+        }
+
+        return dp[2][n - 1];
     }
 };
