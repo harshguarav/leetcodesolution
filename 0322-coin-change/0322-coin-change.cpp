4c00bc0453efe0d77a4890d82c3aeced29e27d6a
harsh kumar jha <115914239+harshguarav@users.noreply.github.com>
2025-07-09 14:45:24 +0530

    Time: 15 ms (93.47%), Space: 17.9 MB (73.68%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(), coins.end());  // Sort to safely use coins[m]
-        vector<int> dp(amount + 1, amount + 1); 
+        sort(coins.begin(), coins.end());  // ensure coins are sorted
+        vector<int> dp(amount + 1, -1);
         dp[0] = 0;
 
-        // Fill initial unreachable values with -1
         int idx = 1;
         while (idx < coins[0] && idx <= amount) {
             dp[idx] = -1;
@@ -15,21 +14,26 @@ public:
         int m = 0;
 
         for (int i = idx; i <= amount; ++i) {
-            // If current i is exactly equal to coin[m], we need only 1 coin
             if (m < coins.size() && i == coins[m]) {
                 dp[i] = 1;
                 m++;
-            } 
-            else {
-                dp[i] = -1; // assume not possible
+            } else {
+                int minVal = INT_MAX;
+                int k = 0;
 
-                for (int j = 0; j < coins.size(); j++) {
-                    int prev = i - coins[j];
-                    if (prev >= 0 && dp[prev] != -1) {
-                        if (dp[i] == -1) dp[i] = dp[prev] + 1;
-                        else dp[i] = min(dp[i], dp[prev] + 1);
+                // manually check coins less than current amount i
+                while (k < coins.size() && coins[k] < i) {
+                    int prev = i - coins[k];
+                    if (dp[prev] != -1) {
+                        minVal = min(minVal, dp[prev] + 1);
                     }
+                    k++;
                 }
+
+                if (minVal != INT_MAX)
+                    dp[i] = minVal;
+                else
+                    dp[i] = -1;
             }
         }
 
