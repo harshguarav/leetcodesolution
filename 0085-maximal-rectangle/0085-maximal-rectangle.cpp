00a6b6fe354c93cf5a2e3ccb345cb917b1cc16ce
harsh kumar jha <115914239+harshguarav@users.noreply.github.com>
2025-07-10 01:16:38 +0530

    Time: 15 ms (21.78%), Space: 18.9 MB (54.89%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         stack<int> st;
-        heights.push_back(0); // sentinel to empty the stack at the end
         int maxArea = 0;
+        int n = heights.size();
 
-        for (int i = 0; i < heights.size(); ++i) {
-            while (!st.empty() && heights[i] < heights[st.top()]) {
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i];
+            while (!st.empty() && currHeight < heights[st.top()]) {
                 int h = heights[st.top()];
                 st.pop();
-                int width = st.empty() ? i : (i - st.top() - 1);
+                int width = st.empty() ? i : i - st.top() - 1;
                 maxArea = max(maxArea, h * width);
             }
             st.push(i);
@@ -18,31 +19,30 @@ public:
     }
 
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty()) return 0;
-        
         int row = matrix.size();
+        if (row == 0) return 0;
         int col = matrix[0].size();
         vector<vector<int>> dp(row, vector<int>(col, 0));
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (matrix[i][j] == '1') {
-                    if (i == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = dp[i - 1][j] + 1;
-                    }
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                if (matrix[j][i] == '0') {
+                    dp[j][i] = 0;
                 } else {
-                    dp[i][j] = 0;
+                    if (j == 0) {
+                        dp[j][i] = 1;
+                    } else {
+                        dp[j][i] = 1 + dp[j - 1][i];
+                    }
                 }
             }
         }
 
-        int maxRect = 0;
+        int maxArea = 0;
         for (int i = 0; i < row; i++) {
-            maxRect = max(maxRect, largestRectangleArea(dp[i]));
+            maxArea = max(maxArea, largestRectangleArea(dp[i]));
         }
 
-        return maxRect;
+        return maxArea;
     }
 };
