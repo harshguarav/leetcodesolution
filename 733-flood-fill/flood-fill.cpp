67ee7fd4fa11c70f8a0409384c4bf399d2a67738
harsh kumar jha <115914239+harshguarav@users.noreply.github.com>
2025-08-12 21:30:52 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (76.83%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> & image ,int x,int y,int color,int ori){
-        int n=image.size();
-        int m=image[0].size();
-        if(x<0 || x >=n || y<0 || y>=m || image[x][y]!=ori) return;
-        image[x][y]=color;
-        dfs(image,x-1,y,color,ori);
-        dfs(image,x+1,y,color,ori);
+    void dfs(vector<vector<int>>& image,int x,int y,int color,int ori){
+    int n=image.size();
+    int m=image[0].size();
+    if(x<0 || y<0 || x>=n ||y>=m || image[x][y]!=ori) return;
+    image[x][y]=color;
 
-        dfs(image,x,y-1,color,ori);
+    dfs(image,x-1,y,color,ori);
+    dfs(image,x,y-1,color,ori);
 
-        dfs(image,x,y+1,color,ori);
+    dfs(image,x+1,y,color,ori);
 
-    }
+    dfs(image,x,y+1,color,ori);
+
+}
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int ori=image[sr][sc];
-        if(ori==color) return image;
+        if(ori==color){
+            return image;
+        }
+        
         dfs(image,sr,sc,color,ori);
         return image;
         
