07742442665562cfadd9e138b89bf93b3127ec10
harsh kumar jha <115914239+harshguarav@users.noreply.github.com>
2025-11-04 01:57:43 +0530

    Time: 21 ms (90.55%) | Memory: 151.4 MB (91.22%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -1,54 +1,99 @@
+// union find class with Size & components
+class UnionFind {
+    int root[100001], Size[100001];
+public:
+    int components;
+    //Constructor using initializer list
+    UnionFind(int n): components(n+1){
+        fill(Size, Size+n+1, 1);
+        iota(root, root+n+1, 0);//[0,1,...,n]    
+    }
+    
+    int Find(int x) {//Path Compression O(alpha(n))
+        if (x==root[x]) 
+            return x;
+        return root[x] = Find(root[x]);
+    }
+
+    bool Union(int x, int y) { //Union by Size O(alpha(n))   
+        x=Find(x), y=Find(y);
+        
+        if (x == y) return 0;
+        
+        if (Size[x] > Size[y]) {
+            Size[x] +=Size[y];
+            root[y] = x;
+        } 
+        else {
+            Size[y] += Size[x];
+            root[x] = y;
+        }       
+        components--;
+        return 1;
+    }     
+};
+
 class Solution {
 public:
-    // this function will mark all cells seen by one guard
-    void counter(vector<vector<char>> &visited, int i, int j) {
-        int m = visited.size();
-        int n = visited[0].size();
-
-        // up
-        for (int x = i - 1; x >= 0; x--) {
-            if (visited[x][j] == 'W' || visited[x][j] == 'G') break;
-            if (visited[x][j] == 'F') visited[x][j] = 'T';
+    bitset<100000> grid=0;
+    int m, n, N;
+    inline int  idx(int r, int c){
+        return r*n+c;
+    }
+    void cross(int r, int c, UnionFind& G) {
+        // downwards
+        for (int i = r + 1; i < m; i++) {
+            if (grid[idx(i, c)]) break;
+            G.Union(idx(i, c), N); // connect to N
         }
-
-        // down
-        for (int x = i + 1; x < m; x++) {
-            if (visited[x][j] == 'W' || visited[x][j] == 'G') break;
-            if (visited[x][j] == 'F') visited[x][j] = 'T';
+        // upwards
+        for (int i = r - 1; i >= 0; i--) {
+            if (grid[idx(i, c)]) break;
+            G.Union(idx(i, c), N); // connect to N
         }
-
-        // left
-        for (int y = j - 1; y >= 0; y--) {
-            if (visited[i][y] == 'W' || visited[i][y] == 'G') break;
-            if (visited[i][y] == 'F') visited[i][y] = 'T';
+        // rightwards
+        for (int j = c + 1; j < n; j++) {
+            if (grid[idx(r, j)]) break;
+            G.Union(idx(r, j), N); // connect to N
         }
-
-        // right
-        for (int y = j + 1; y < n; y++) {
-            if (visited[i][y] == 'W' || visited[i][y] == 'G') break;
-            if (visited[i][y] == 'F') visited[i][y] = 'T';
+        // leftwards
+        for (int j = c - 1; j >= 0; j--) {
+            if (grid[idx(r, j)]) break;
+            G.Union(idx(r, j), N); // connect to N
         }
     }
 
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<char>> visited(m, vector<char>(n, 'F'));
+        this->m = m, this->n = n;
+        N=n*m;
+        UnionFind G(N);
 
-        // mark guards and walls
-        for (auto &pos : guards) visited[pos[0]][pos[1]] = 'G';
-        for (auto &pos : walls) visited[pos[0]][pos[1]] = 'W';
-
-        // call counter for every guard
-        for (auto &pos : guards) {
-            int i = pos[0], j = pos[1];
-            counter(visited, i, j);
+        // Mark walls
+        for (auto& ij : walls){
+            int r = ij[0], c = ij[1];
+            grid[idx(r, c)] = 1;
+            G.components--;
         }
-
-        // count unguarded cells
-        int unguarded = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (visited[i][j] == 'F') unguarded++;
-
-        return unguarded;
+        // Mark guards 
+        for (auto& ij : guards){
+            int r = ij[0], c = ij[1];
+            grid[idx(r, c)] = 1;
+            G.components--;
+        }
+        // connect guarded Cells to N
+        for (auto& ij : guards) {
+            int r = ij[0], c = ij[1];
+            cross(r, c, G);
+        }
+        return G.components-1;
     }
 };
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
